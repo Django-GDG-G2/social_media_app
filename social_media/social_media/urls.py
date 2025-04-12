2908fb9a72ef9bf9be4c49a4acfbffabf28c0ab4
Yared <149290215+yared-Habtamu@users.noreply.github.com>
2025-04-12 15:18:37 +0300

    Swagger Configured

--- social_media/social_media/urls.py
+++ social_media/social_media/urls.py
@@ -15,16 +15,32 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework_simplejwt.views import TokenRefreshView
-from users.views import UserLoginView
+# from users.views import UserLoginView
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your API",
+        default_version='v1',
+        description="API Documentation",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
     path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include('posts.urls')),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+    # Swagger/OpenAPI Docs
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
